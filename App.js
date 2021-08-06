46b47cdc9a2cb2faf07baa9ca401de57b37941f4
Abhishek Raj <abhishek@betatestsolutions.com>
2021-08-06 12:09:42 +0530

    Initial commit with required dependency added.

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import type {Node} from 'react';
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>Welcome to ToDo App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
